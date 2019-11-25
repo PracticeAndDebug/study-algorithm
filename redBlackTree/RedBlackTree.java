17b67d1af5d3b74b998f2e78e8c9b2da525edf95
PracticeAndDebug <ncpocketmonster@gmail.com>
2019-11-25 23:21:26 +0800

    start to write red black tree

---
+++ redBlackTree/RedBlackTree.java
@@ -0,0 +1,14 @@
+package redBlackTree;
+
+import redBlackTree.RedBlackTree;
+
+class RedBlackTreeMethod{
+
+  public boolean insert( RedBlackTree root , int key ){
+     
+  } 
+
+  public int [] traverse( RedBlackTree root ){ // mid traverse
+    
+  }
+}
---
+++ redBlackTree/RedBlackTreeNode.java
@@ -0,0 +1,11 @@
+package redBlackTree;
+
+import redBlackTree.RedBlackTree;
+import redBlackTree.RedBlackTreeMethod;
+
+public class RedBlackTreeTest{
+  public boolean checkItOut(){
+    return true;
+  }
+
+}
